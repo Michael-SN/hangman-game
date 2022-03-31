634b7e52d6ec928bceb068d05f56d9129192b644
Michael Nascimento <michael.dev.nascimento@gmail.com>
2022-03-31 17:57:06 -0300

    Estrutura iniciada, projeto em andamento

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div class="header-game">
+      <h1>Hangman Game</h1>
+    </div>
+
+    <section id="start" v-if="screen === 'start'">
+      <Formulario v-if="step === 'word'"
+        title="Define the word"
+        button='Next'        
+        :action="defineWord"
+      />
+
+      <Formulario v-if="step === 'tip'"
+        title="Define the Tip"
+        button='Next'        
+      />
+    </section>
+    <section id="game" v-if="screen === 'game'">
+      Playing
+    </section>
+  </div>
+</template>
+
+<script>
+import './css/global.css'
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'App',  
+  data() {
+    return {
+      screen :'start',
+      step: 'word',
+      word: '',
+      tip: ''
+    }
+  },
+  components: {
+    Formulario    
+  },
+  methods: {
+    defineWord(word) {
+      this.word = word
+      this.step = 'tip'
+    },
+    defineTip(tip) {
+      this.tip = tip
+      this.screen= 'game',
+      this.step = 'game'     
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="form">
+    <div class="form-title">
+      <h3>
+        {{ title }}
+      </h3>
+    </div>
+    <input type="text" class="form-input" v-model="inputValue">
+    <button
+      :disable="(inputValue.length === 0)"
+      @click="onSubmit"      
+      >{{ button }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Formulario',
+  props: {
+    title: String,
+    button: String,
+    action: Function
+  },
+  data() {
+    return {
+      inputValue: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.action(this.inputValue)
+      this.inputValue = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.form-input {
+  padding: 10px 5px;
+  border: 0;
+  border-radius: 5px;
+  margin: 10px 0;
+  text-align: center;
+  color: var(--color-text-dark);
+  outline: none;  
+  transition: all .2s;  
+  color: #585858;
+  font-weight: bold;  
+  font-family: inherit;
+}
+</style>
